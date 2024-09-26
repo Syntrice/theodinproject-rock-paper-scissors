b5ce014452679bcac0c58fc409cdc383d5e21534
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-09-26 17:59:45 +0100

    Create function to get choice from user
    The function gets the user's choice of "rock", "paper", or "scissors"
    by displaying a prompt where the user can input text. The input is not
    case sensitive, as the input is converted to lowercase before being
    checked.

--- javascript.js
+++ javascript.js
@@ -7,3 +7,17 @@ function getComputerChoice() {
     let index = Math.floor(Math.random() * 3)
     return choices[index]
 }
+
+function getHumanChoice() {
+    let choice = prompt("Enter your choice [rock, paper, scissors]:")
+    /* we don't want the input to be case sensitive */
+    choice = choice.toLowerCase()
+    switch (choice) {
+        case "rock":
+            return choices[0]
+        case "paper":
+            return choices[1]
+        case "scissors":
+            return choices[2]
+    }
+}
