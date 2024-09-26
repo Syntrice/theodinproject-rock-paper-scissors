de379e4f47d47eb9bbb89bb75224f99e9b2ebc28
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-09-26 18:09:12 +0100

    Refactor code to use an object to store choices
    By storing the choices as object properties, it allows the the property
    values to be set to the loosing object, or the beaten object according
    to the rock paper scissors rules. This will make it very easy to
    determine winner when that functionality gets implemented later. The
    functions for getting choices have been updated to reflect this change,
    using the Object.keys(object) method to get the attribute names in
    array format.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,21 @@
-const choices = ["Rock", "Paper", "Scissors"]
+
+const choices = {"rock" : "scissors", "paper" : "rock", "scissors" : "paper"}
 
 function getComputerChoice() {
 
     /** we need to use Math.random() and Math.floor to generate integers in 
      * a range */ 
     let index = Math.floor(Math.random() * 3)
-    return choices[index]
+    return Object.keys(choices)[index]
 }
 
 function getHumanChoice() {
     let choice = prompt("Enter your choice [rock, paper, scissors]:")
     /* we don't want the input to be case sensitive */
     choice = choice.toLowerCase()
-    switch (choice) {
-        case "rock":
-            return choices[0]
-        case "paper":
-            return choices[1]
-        case "scissors":
-            return choices[2]
+    if (Object.keys(choices).includes(choice)) {
+        return choice
     }
 }
+
+console.log(getHumanChoice())
