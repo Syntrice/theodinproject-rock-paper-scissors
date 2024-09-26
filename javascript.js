e194e1b46c417f3f72152495d924ebbe5c0a6e10
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-09-26 17:51:19 +0100

    Add a function to generate a random choice
    The function returns a random rock paper scissors choice from an array
    containing each string. It does this using the Math.floor() function,
    along with Math.random() to generate a random index between 0 and 2.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+const choices = ["Rock", "Paper", "Scissors"]
+
+function getComputerChoice() {
+
+    /** we need to use Math.random() and Math.floor to generate integers in 
+     * a range */ 
+    let index = Math.floor(Math.random() * 3)
+    return choices[index]
+}
