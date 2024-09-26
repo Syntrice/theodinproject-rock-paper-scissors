84a4427de9f911585b1a07a7f689a39cce144873
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-09-26 18:43:00 +0100

    Refactor code and implement playGame function
    Refactor the playRound function and score variables to within a new
    function, playGame(rounds = 5). This function will play a number of
    rounds, defaulting to 5. It handles invalid input for user, and only
    advances a round if the round was not a draw. To determine this, the
    playRound() function returns true for valid rounds, and false for draws.
    
    There was also a small typo in the playRound function where
    computerChoice variable was getting incremented instead of
    computerScore.

--- javascript.js
+++ javascript.js
@@ -1,44 +1,78 @@
 
-const choices = {"rock" : "scissors", "paper" : "rock", "scissors" : "paper"}
-let humanScore = 0
-let computerScore = 0
+const choices = {"rock" : "scissors", "paper" : "rock", "scissors" : "paper"};
 
 function getComputerChoice() {
 
     /** we need to use Math.random() and Math.floor to generate integers in 
      * a range */ 
-    let index = Math.floor(Math.random() * 3)
-    return Object.keys(choices)[index]
+    let index = Math.floor(Math.random() * 3);
+    return Object.keys(choices)[index];
 }
 
 function getHumanChoice() {
-    let choice = prompt("Enter your choice [rock, paper, scissors]:")
+    let choice = prompt("Enter your choice [rock, paper, scissors]:");
     /* we don't want the input to be case sensitive */
-    choice = choice.toLowerCase()
+    choice = choice.toLowerCase();
     if (Object.keys(choices).includes(choice)) {
-        return choice
+        return choice;
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (choices[humanChoice] === computerChoice) {
-        console.log(`You win! ${humanChoice} beats ${choices[humanChoice]}!`)
-        humanScore++
-    } else if (choices[computerChoice] === humanChoice) {
-        console.log(`You loose! ${computerChoice} beats ${choices[computerChoice]}!`)
-        computerChoice++
+function playGame(rounds = 5) {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // Function to play a round. Will return false if round is a draw, otherwise true
+    function playRound(humanChoice, computerChoice) {
+        if (choices[humanChoice] === computerChoice) {
+            console.log(`You win! ${humanChoice} beats ${choices[humanChoice]}!`);
+            humanScore++;
+            return true
+        } else if (choices[computerChoice] === humanChoice) {
+            console.log(`You loose! ${computerChoice} beats ${choices[computerChoice]}!`);
+            computerScore++
+            return true
+        } else {
+            console.log(`It's a draw! You and the computer both chose ${humanChoice}!`);
+            return false
+        }
+    }
+
+    let currentRound = 0;
+    while (currentRound < rounds) {
+        console.log(`Your score: ${humanScore}`)
+        console.log(`Computer score: ${computerScore}`)
+        let humanChoice = getHumanChoice();
+
+        // clause for invalid human input
+        if (humanChoice === undefined) {
+            console.log("Please enter a valid choice. Possible choices are 'rock', 'paper', or 'scissors'")
+            continue;
+        }
+        let computerChoice = getComputerChoice();
+        
+        console.log(`You chose: ${humanChoice}`);
+        console.log(`Computer chose: ${computerChoice}`);
+
+        let isDraw = playRound(humanChoice, computerChoice);
+        if (isDraw) {
+            currentRound++
+        }
+    }
+
+    console.log("Game over!")
+    console.log(`You scored: ${humanScore}`)
+    console.log(`Computer scored: ${computerScore}`)
+
+    if (humanScore > computerScore) {
+        console.log("You win!")
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins!")
     } else {
-        console.log(`It's a draw! You and the computer both chose ${humanChoice}!`)
+        console.log("You and computer drew!")
     }
+
 }
 
-while (true) {
-    let humanChoice = getHumanChoice()
-    console.log(`You chose: ${humanChoice}`)
-
-    let computerChoice = getComputerChoice()
-    console.log(`Computer choses: ${computerChoice}`)
-
-    playRound(humanChoice, computerChoice)
-}
-
+playGame()
