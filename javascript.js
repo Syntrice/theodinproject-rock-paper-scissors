3bfeb9252dea5e6bb9ee579625f32ce2f8282724
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-10-18 10:05:42 +0100

    Reimplement multiple round functionality
    The game will now again keep track of rounds and score, and announce
    the winner with the highest score once the number of rounds is reached.
    At this point, the buttons no longer do anything as the game has ended.

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ const btnPaper = document.querySelector("#btn-paper");
 const btnScissors = document.querySelector("#btn-scissors");
 
 const results = document.querySelector("#results");
-results.textContent = "Click on a button to select a choice";
+const score = document.querySelector("#score");
 
 function getComputerChoice() {
     /** we need to use Math.random() and Math.floor to generate integers in
@@ -14,35 +14,61 @@ function getComputerChoice() {
     return Object.keys(choices)[index];
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your choice [rock, paper, scissors]:");
-    /* we don't want the input to be case sensitive */
-    choice = choice.toLowerCase();
-    if (Object.keys(choices).includes(choice)) {
-        return choice;
+function playGame(rounds = 5) {
+    let humanScore = 0;
+    let computerScore = 0;
+    let currentRound = 0;
+
+    results.textContent = "Click on a button to select a choice";
+
+    // Function to play a round. Will return true if round is a draw, otherwise
+    function playRound(humanChoice, computerChoice) {
+        // Only play a round if the game is still ongoing
+        if (currentRound >= rounds) {
+            return;
+        }
+
+        if (choices[humanChoice] === computerChoice) {
+            results.textContent = `You win the round! ${humanChoice} beats ${choices[humanChoice]}!`;
+            humanScore++;
+        } else if (choices[computerChoice] === humanChoice) {
+            results.textContent = `You loose the round! ${computerChoice} beats ${choices[computerChoice]}!`;
+            computerScore++;
+        } else {
+            results.textContent = `The round is a draw! You and the computer both chose ${humanChoice}!`;
+        }
+
+        currentRound++;
+        updateScoreText();
     }
+
+    function updateScoreText() {
+        if (currentRound < rounds) {
+            score.textContent = `Your Score: ${humanScore} Computer Score: ${computerScore}`;
+        } else {
+            // if the game has ended
+
+            if (humanScore > computerScore) {
+                score.textContent = "Game Over! You win!";
+            } else if (computerScore > humanScore) {
+                score.textContent = "Game Over! You loose!";
+            } else {
+                score.textContent = "Game Over! It was a draw!";
+            }
+        }
+    }
+
+    btnRock.addEventListener("click", () => {
+        playRound("rock", getComputerChoice());
+    });
+
+    btnPaper.addEventListener("click", () => {
+        playRound("paper", getComputerChoice());
+    });
+
+    btnScissors.addEventListener("click", () => {
+        playRound("scissors", getComputerChoice());
+    });
 }
 
-// Function to play a round. Will return false if round is a draw, otherwise true
-function playRound(humanChoice, computerChoice) {
-    if (choices[humanChoice] === computerChoice) {
-        results.textContent = `You win! ${humanChoice} beats ${choices[humanChoice]}!`;
-        return true;
-    } else if (choices[computerChoice] === humanChoice) {
-        results.textContent = `You loose! ${computerChoice} beats ${choices[computerChoice]}!`;
-        return true;
-    } else {
-        results.textContent = `It's a draw! You and the computer both chose ${humanChoice}!`;
-        return false;
-    }
-}
-
-btnRock.addEventListener("click", () => {
-    playRound("rock", getComputerChoice());
-});
-btnPaper.addEventListener("click", () => {
-    playRound("paper", getComputerChoice());
-});
-btnScissors.addEventListener("click", () => {
-    playRound("scissors", getComputerChoice());
-});
+playGame();
